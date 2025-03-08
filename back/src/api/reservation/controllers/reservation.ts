c40dbee855a55b93625b60ba9de330d36cb7641b
paulwecker <paulwecker504@gmail.com>
2025-03-08 17:32:23 -0600

    find relevant owned and public

--- back/src/api/reservation/controllers/reservation.ts
+++ back/src/api/reservation/controllers/reservation.ts
@@ -4,4 +4,28 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::reservation.reservation');
+export default factories.createCoreController('api::reservation.reservation', {
+  async find(ctx) {
+    if (ctx.state.user) {
+      const entries = await strapi.entityService.findMany('api::reservation.reservation', {
+        filters: {
+          in: {
+            $gt: new Date()
+          },
+          $or: [
+            {
+              stat: 'confirmed'
+            },
+            {
+              stat: 'requested',
+              user: ctx.state.user.id
+            }
+          ]
+        },
+      });
+      return ctx.send(entries);
+    } else {
+      return ctx.send();
+    }
+  }
+});
